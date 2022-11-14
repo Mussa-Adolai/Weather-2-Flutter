20edfdf788a9bdc930965d974362c795b6f45b72
Mussa-Adolai <60032727+Mussa-Adolai@users.noreply.github.com>
2022-11-14 23:40:21 +0300

    TextField

--- lib/home.dart
+++ lib/home.dart
@@ -30,6 +30,7 @@ class currentWeather extends StatefulWidget {
 class _currentWeatherState extends State<currentWeather> {
   @override
   Widget build(BuildContext context) {
+    var fNode = FocusNode();
     return GestureDetector(
       child: GlowContainer(
         height: MediaQuery.of(context).size.height - 230,
@@ -41,6 +42,18 @@ class _currentWeatherState extends State<currentWeather> {
           bottomRight: Radius.circular(68.0),
         ),
         color: Color(0xff00A1ff),
+        spreadRadius: 5.0,
+        child: TextField(
+          focusNode: fNode,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.0),
+              ),
+            ),
+            hintText: 'Enter a city',
+          ),
+        ),
       ),
     );
   }
